40d9daa2b5e9edb34fbd69d1166226c11442e0be
malavikaac <acmalavika@gmail.com>
2023-03-09 09:42:38 +0530

    incomplete and error

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,29 @@
 import 'package:flutter/material.dart';
 import 'package:responsive/pages/home.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+
 
 void main() {
-  runApp(MyApp());
+  WidgetsFlutterBinding.ensureInitialized();
+
+  if (Platform.isWindows || Platform.isLinux || Platform.isMacOS) {
+    setWindowTitle('My App');
+    setWindowMaxSize(const Size(max_width, max_height));
+    setWindowMinSize(const Size(min_width, min_height));
+  }
+
+  runApp(const MyApp());
 }
-class MyApp extends StatelessWidget {
+
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,24 +1,66 @@
 import 'package:flutter/material.dart';
 
-
-class HomePage  extends StatelessWidget {
-  const HomePage ({super.key});
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.amber[100],
-      body: Center(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            Text('height: ' + MediaQuery.of(context).size.height.toString()),
-             Text('Width: ' + MediaQuery.of(context).size.height.toString()),
-              Text('Aspect ratio: ' + MediaQuery.of(context).size.height.toStringAsFixed(2)),
-              Text(MediaQuery.of(context).orientation.toString()),
-          ],
-        ),),
+      backgroundColor: Color.fromARGB(255, 204, 240, 231),
+      body: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.all(100.0),
+            child: Container(
+              width: MediaQuery.of(context).size.width / 2,
+              height: MediaQuery.of(context).size.height / 2,
+              decoration:
+                  BoxDecoration(color: Color.fromARGB(255, 155, 241, 201)),
+              child: Column(
+                children: [
+                  //text==============
+                  Text(
+                    "Life Queto ",
+                    style: TextStyle(
+                      fontStyle: FontStyle.normal,
+                      fontWeight: FontWeight.w700,
+                      fontSize: 31.25,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Text(
+                    "Keep smiling, because life is a beautiful thing and there's so much to smile about.",
+                    style: TextStyle(
+                      fontStyle: FontStyle.normal,
+                      fontWeight: FontWeight.w400,
+                      fontSize: 16.0,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(right: 100),
+                    child: Text(
+                      "Love the life you live",
+                      style: TextStyle(
+                        fontStyle: FontStyle.normal,
+                        fontWeight: FontWeight.w400,
+                        fontSize: 16.0,
+                      ),
+                    ),
+                  ),
+                  //text=================
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
